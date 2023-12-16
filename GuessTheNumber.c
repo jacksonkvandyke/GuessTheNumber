595a2fc4181256f071ef34db433bd50496390964
jacksonkvandyke <102544560+jacksonkvandyke@users.noreply.github.com>
2023-12-15 19:14:22 -0600

    first commit

---
+++ GuessTheNumber.c
@@ -0,0 +1,52 @@
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+    //Get random value
+    srand(time(NULL));
+    int randomValue = rand() % 100;
+
+    //Prompt user
+    printf("%s", "Hello, welcome to my little game, please enter a random number to get started! :) ");
+    printf("%s", "\n Enter a value: ");
+    
+    //Get user value
+    int userValue;
+    scanf("%d", &userValue);
+    
+    //Check if it is equal to the random value
+    while (userValue != randomValue){
+        if (userValue < randomValue){
+            if (randomValue - userValue > 10)
+                printf("%s", "\n That was not the right value, try a larger value: ");
+            else{
+                printf("%s", "\n That was close, just a bit larger: ");
+
+            }
+            
+            scanf("%d", &userValue);
+
+        }
+
+        if (userValue > randomValue){
+            if (userValue - randomValue > 10){
+                printf("%s", "\n That was not the right value, try a smaller value: ");
+
+            }else {
+                printf("%s", "\n That was close, just a bit smaller: ");
+
+            }
+            scanf("%d", &userValue);
+
+        }
+
+    }
+
+    //Congratulate the user for finding the correct value
+    printf("%s%d%s", "\n Great job, you found the correct value of ", userValue, ", thanks for playing!");
+    
+    return 0;
+
+}
---
+++ readMe.txt
@@ -0,0 +1,3 @@
+Title: GuessTheNumber
+Description: This is a minimal minigame giving you the chance to guess the value!
+How to play: Simply run the "GuessTheNumber.exe" file and play!
